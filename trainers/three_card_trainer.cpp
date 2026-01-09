0e25575f66cdd6aec5215884ef5ae1f1bef1b912
root <giantcheeseguy2@gmail.com>
2026-01-09 14:46:37 +0000

    new model

--- trainers/three_card_trainer.cpp
+++ trainers/three_card_trainer.cpp
@@ -23,8 +23,8 @@ struct TrainConfig {
 int main() {
     TrainConfig config;
     config.seed = 42;
-    config.player0_policy = "./final_model/player.bin";
-    config.player1_policy = "./final_model/player.bin";
+    config.player0_policy = "";
+    config.player1_policy = "";
     config.output_dir = "./three_card_models";
     config.iterations = 10'000'000;
 
@@ -42,8 +42,7 @@ int main() {
         config.checkpoint_every_secs,
         config.player0_policy,
         config.player1_policy,
-        config.output_dir,
-        5277328);
+        config.output_dir);
 
     return 0;
 }
