7a45a6fc74b82039910780268e029a15c6cb31b2
thomas vm <giantcheeseguy2@gmail.com>
2026-01-10 10:13:53 +0000

    new model

--- trainers/three_card_trainer.cpp
+++ trainers/three_card_trainer.cpp
@@ -14,7 +14,7 @@ struct TrainConfig {
     int seed = 0;
     int iterations = 1'000;
     float log_every_secs = 5.0f;
-    float checkpoint_every_secs = 300.0f;
+    float checkpoint_every_secs = 1800.0f;
     std::string output_dir;
     std::string player0_policy;
     std::string player1_policy;
@@ -22,9 +22,9 @@ struct TrainConfig {
 
 int main() {
     TrainConfig config;
-    config.seed = 42;
-    config.player0_policy = "";
-    config.player1_policy = "";
+    config.seed = 42138757;
+    config.player0_policy = "./final_model/player-5-hr.bin";
+    config.player1_policy = "./final_model/player-5-hr.bin";
     config.output_dir = "./three_card_models";
     config.iterations = 10'000'000;
 
@@ -43,7 +43,8 @@ int main() {
         config.checkpoint_every_secs,
         config.player0_policy,
         config.player1_policy,
-        config.output_dir);
+        config.output_dir,
+	860000);
 
     return 0;
 }
