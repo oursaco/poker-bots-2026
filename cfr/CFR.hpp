23686740deafded2d9c0ede15a494c532189e63a
rl551 <raymondluo551@gmail.com>
2026-01-10 21:26:47 -0500

    added BR eval during training for multi-threaded

--- cfr/CFR.hpp
+++ cfr/CFR.hpp
@@ -10,20 +10,11 @@
 #include <cstdint>
 #include <future>
 #include <limits>
-#include <memory>
 #include <mutex>
 #include <string>
 #include <vector>
 #include <cassert>
-#ifdef _OPENMP
 #include <omp.h>
-#else
-// Allow building without OpenMP; pragmas will be ignored and these calls fall back to single-thread defaults.
-inline int omp_get_num_procs(){ return 1; }
-inline int omp_get_max_threads(){ return 1; }
-inline int omp_get_num_threads(){ return 1; }
-inline int omp_get_dynamic(){ return 0; }
-#endif
 #include <algorithm>
 #include <unordered_map>
 #include "constants/constants.h"
@@ -294,6 +285,52 @@ struct MultiDCFRTrainer : CFRTrainer {
                 last_checkpoint_time = cur_time;
                 players[0].savePolicy(checkpoint_dir + "/player0_" + to_string(i) + ".bin");
                 players[1].savePolicy(checkpoint_dir + "/player1_" + to_string(i) + ".bin");
+
+                // If an evaluation is still running, don't start another one.
+                if(br_in_flight){
+                    if(br_future.valid() && br_future.wait_for(std::chrono::seconds(0)) == std::future_status::ready){
+                        br_future.get();
+                        br_in_flight = false;
+                    } else {
+                        cout << "Skipping BR eval at iteration " << i << " (previous eval still running)" << endl;
+                    }
+                }
+
+                if(!br_in_flight){
+                    // Snapshot policies so BR thread reads an immutable copy.
+                    DCFRPolicy p0_snapshot = players[0];
+                    DCFRPolicy p1_snapshot = players[1];
+                    GameTree* br_tree_ptr = br_tree.get();
+                    const int eval_iter = i;
+                    const int eval_seed = seed ^ (int)((uint32_t)i * 0x9E3779B9u);
+                    const int eval_samples = 100000;
+
+                    br_future = std::async(std::launch::async, [=]() mutable {
+                        auto t0 = chrono::high_resolution_clock::now();
+                        BestResponseEvaluator evaluator;
+                        evaluator.setTree(br_tree_ptr);
+
+                        // BR vs player0 (SB) strategy -> value from player1 (BB) perspective
+                        BestResponseResult br_bb = evaluator.computeBestResponse(p0_snapshot, 0, eval_seed, eval_samples);
+                        // BR vs player1 (BB) strategy -> value from player0 (SB) perspective
+                        BestResponseResult br_sb = evaluator.computeBestResponse(p1_snapshot, 1, eval_seed + 1, eval_samples);
+
+                        float nashconv = br_sb.value + br_bb.value;
+                        float exploitability = 0.5f * nashconv;
+                        auto secs = chrono::duration_cast<chrono::seconds>(chrono::high_resolution_clock::now() - t0).count();
+
+                        lock_guard<std::mutex> lock(br_cout_mutex);
+                        cout.setf(std::ios::fixed);
+                        cout.precision(6);
+                        cout << "[BR @ iter " << eval_iter << "] "
+                             << "BR(SB)=" << br_sb.value << " "
+                             << "BR(BB)=" << br_bb.value << " "
+                             << "NashConv=" << nashconv << " "
+                             << "Exploitability=" << exploitability << " "
+                             << "(" << secs << "s)" << endl;
+                    });
+                    br_in_flight = true;
+                }
             }
         }
         cout << "Finished training in " << chrono::duration_cast<chrono::seconds>(chrono::high_resolution_clock::now() - start_time).count() << " seconds" << endl;
@@ -409,11 +446,8 @@ struct DCFRTrainer : CFRTrainer {
 
                 if(!br_in_flight){
                     // Snapshot policies so BR thread reads an immutable copy.
-                    //
-                    // IMPORTANT: DCFRPolicy is ~12MB (arrays of size POLICY_SZ). Copying it onto the
-                    // stack can overflow the default thread stack and segfault. Snapshot on the heap.
-                    auto p0_snapshot = std::make_shared<DCFRPolicy>(players[0]);
-                    auto p1_snapshot = std::make_shared<DCFRPolicy>(players[1]);
+                    DCFRPolicy p0_snapshot = players[0];
+                    DCFRPolicy p1_snapshot = players[1];
                     GameTree* br_tree_ptr = br_tree.get();
                     const int eval_iter = i;
                     const int eval_seed = seed ^ (int)((uint32_t)i * 0x9E3779B9u);
@@ -425,9 +459,9 @@ struct DCFRTrainer : CFRTrainer {
                         evaluator.setTree(br_tree_ptr);
 
                         // BR vs player0 (SB) strategy -> value from player1 (BB) perspective
-                        BestResponseResult br_bb = evaluator.computeBestResponse(*p0_snapshot, 0, eval_seed, eval_samples);
+                        BestResponseResult br_bb = evaluator.computeBestResponse(p0_snapshot, 0, eval_seed, eval_samples);
                         // BR vs player1 (BB) strategy -> value from player0 (SB) perspective
-                        BestResponseResult br_sb = evaluator.computeBestResponse(*p1_snapshot, 1, eval_seed + 1, eval_samples);
+                        BestResponseResult br_sb = evaluator.computeBestResponse(p1_snapshot, 1, eval_seed + 1, eval_samples);
 
                         float nashconv = br_sb.value + br_bb.value;
                         float exploitability = 0.5f * nashconv;
