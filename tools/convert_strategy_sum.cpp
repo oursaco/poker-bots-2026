2821a6aad9cabe986d0a77c550a6d3c9ea34a1e1
oursaco <giantcheeseguy2@gmail.com>
2026-01-12 19:03:43 -0500

    fix

--- tools/convert_strategy_sum.cpp
+++ tools/convert_strategy_sum.cpp
@@ -8,9 +8,11 @@ int main() {
     float iterations = 10000000;
     DCFRPolicy policy;
     policy.loadPolicy(target_path);
+    cout << "states: " << policy.state_count << endl;
     for(int i = 0; i < policy.state_count; i++){
         policy.strategy_sum[i] /= iterations*iterations;
     }
     policy.savePolicy(target_path);
+    cout << "done" << endl;
     return 0;
 }
