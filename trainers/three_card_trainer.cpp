65d6e2fc214a0685c701d2888e0b8d56c574d96a
root <giantcheeseguy2@gmail.com>
2026-01-24 21:05:26 +0000

    inf beta

--- trainers/three_card_trainer.cpp
+++ trainers/three_card_trainer.cpp
@@ -24,11 +24,11 @@ struct TrainConfig {
 
 int main() {
     TrainConfig config;
-    config.seed = 42;
+    config.seed = 1324132;
     config.player0_policy = "";
     config.player1_policy = "";
     config.output_dir = "./three_card_models";
-    config.iterations = 20'000'000;
+    config.iterations = 100'000'000;
 
     ThreeCardInferenceTree tree1;
     ThreeCardInferenceTree tree2;
